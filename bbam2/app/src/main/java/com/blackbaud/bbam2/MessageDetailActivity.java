217f90fc6a0e2a2b5da10afe673d73aaaf9358cd
Clare Coleman <clare.coleman@blackbaud.com>
2015-07-31 01:18:11 -0500

    link account in menu

--- bbam2/app/src/main/java/com/blackbaud/bbam2/MessageDetailActivity.java
+++ bbam2/app/src/main/java/com/blackbaud/bbam2/MessageDetailActivity.java
@@ -48,13 +48,21 @@ public class MessageDetailActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_add_login:
+                navToLinkAccount();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    // TODO: pull into util class
+    private void navToLinkAccount() {
+        Intent intent = new Intent(this, AppSelection.class);
+        startActivity(intent);
     }
 }
--- bbam2/app/src/main/java/com/blackbaud/bbam2/MessageList.java
+++ bbam2/app/src/main/java/com/blackbaud/bbam2/MessageList.java
@@ -87,25 +87,25 @@ public class MessageList extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_message_list, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_add_login:
+                navToLinkAccount();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
+    // TODO: pull into util class
+    private void navToLinkAccount() {
+        Intent intent = new Intent(this, AppSelection.class);
+        startActivity(intent);
+    }
 }
