1b3cc6a4b56cbb3fdd2c33d462463c990570c18e
Clare Coleman <clare.coleman@blackbaud.com>
2015-07-31 04:17:29 -0500

    bb app styling changes

--- bbam2/app/src/main/java/apps/BBApp.java
+++ bbam2/app/src/main/java/apps/BBApp.java
@@ -1,19 +1,18 @@
 package apps;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Created by avaky on 7/30/15.
  */
 public class BBApp
 {
     public String appName;
+    public int appId;
 
     public BBApp() {}
 
-    public BBApp(String name)
+    public BBApp(String name, int appId)
     {
         this.appName = name;
+        this.appId = appId;
     }
 }
